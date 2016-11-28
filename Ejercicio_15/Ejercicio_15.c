3114b1fefd02a89a64cfbcd9bfea4f87660bcb18
FranciscoGustavo <hidalgo_fco520@outlook.com>
2016-11-27 19:50:42 -0600

    Agrgamos el ejercico 15

--- Ejercicio_15/Ejercicio_15.c
+++ Ejercicio_15/Ejercicio_15.c
@@ -1,4 +1,4 @@
-//Calcular el valor de "Y" cuando "X" cambia del 1 al 10,
+//Calcular el valor de "Y" cuando el valor de "X" cambia del 1 al 10,
 //en la siguiente ecuacion y = x + 12
 
 #include<stdio.h>
@@ -7,13 +7,14 @@
 #define doce 12
 
 int main (){
-    int x = 1;
-    int y;
+    int x = 1, y;
 
     while(x <= 10){
-        y = x +doce;
-        printf("")
+        y = x + doce;
+        printf("Cuando X = %d, Y = %d\n",x,y);
+        x++;
     }
+
 
     system("PAUSE");
     return 0;
